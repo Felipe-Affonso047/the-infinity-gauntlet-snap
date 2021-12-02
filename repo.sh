f761e0070fb380d6607e0e51bb1bc0b5f0b5b6b5
Felipe-Affonso047 <82886318+Felipe-Affonso047@users.noreply.github.com>
2021-12-02 12:00:35 -0500

    Create repo.sh

---
+++ repo.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# create a GitHub repo and a local git repo
+#   and connect them together
+
+# make the directory and cd into it
+mkdir $1
+cd $1
+
+# create the local repo                                                          
+git init --initial-branch=main                                                   
+# then the remote                                                                
+gh repo create $1  --public --gitignore $2  --license "unlicense" --confirm
+git pull origin main
+
+# create README.md
+touch README.md
+echo "# $1" >> README.md
+
+# add the GitHub Action for Linting
+mkdir -p .github/workflows
+cd .github/workflows
+echo $2
+if [ $2 = "Swift" ]; then
+  cp ~/scripts/swift.yml ./swift.yml
+  cd ../..
+  echo''
+  echo "[![SwiftLint](https://github.com/ics4u-1-2021/$1/workflows/SwiftLint/badge.svg)](https://github.com/ics4u-1-2021/$1/actions)" >> ./README.md
+  git add .github/workflows/swift.yml
+else
+  cp ~/scripts/main.yml ./main.yml
+  cd ../..
+  echo ''
+  echo "[![GitHub's Super Linter](https://github.com/ics4u-1-2021/$1/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/ics4u-1-2021/$1/actions)" >> ./README.md
+  git add .github/workflows/main.yml
+fi
+
+# update remote
+git add *
+git commit -m "init"
+git push origin main
