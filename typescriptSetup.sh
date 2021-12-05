086b6c183809f77a5169d3c996552a3d298c1c56
Felipe-Affonso047 <82886318+Felipe-Affonso047@users.noreply.github.com>
2021-12-05 13:01:21 -0500

    Update typescriptSetup.sh

--- typescriptSetup.sh
+++ typescriptSetup.sh
@@ -4,6 +4,8 @@ npm install @types/node --save-dev
 npx tsc --init --rootDir src --outDir build \
 --esModuleInterop --resolveJsonModule --lib es6 \
 --module commonjs --allowJs true --noImplicitAny true
+
 mkdir src
 touch src/index.ts
+
 commitgit typescriptSetup
